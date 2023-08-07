77c904d0e0d613fe829a2bdaf96d1aba88c89270
Mgbaramuko Chidiebube Emmanuel <46859058+Emmytheo@users.noreply.github.com>
2023-08-07 14:29:55 +0000

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-PATH="$PATH:/root/.local/bin"
 docker run -d -p 6379:6379 redislabs/redisearch:2.6.0
 pip install -r requirements.txt
 jina auth login
---
+++ setPath.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -l
+PATH=$PATH:/opt/python/3.10.8/bin
+echo $PATH
